bfaae597fb5aa6d47e6eb268330aeec534ca6326
Amulya Bhagat <amulya.b@gmail.com>
2014-04-23 19:34:38 -0400

    Fixes bugs relating to reflexion and index out of bounds
    I goofed hard on that prior commit! It should work now :)

--- kacring.py
+++ kacring.py
@@ -3,14 +3,12 @@ import matplotlib.pyplot as plt
 import sys
 
 class KacRing(object):
-    self.size = 0
-    self.NUM_GATES = 0
-    self.gate_locs = []
-    self.charges = []
+    size = 0
+    gate_locs = []
+    charges = []
 
     def __init__(self, num_sites, num_gates):
         self.size = num_sites
-        self.NUM_GATES = num_gates
         self.gate_locs = random.sample(xrange(num_sites), num_gates)
         for i in xrange(self.size):
             self.charges.append(Charge(self, i))        
@@ -21,16 +19,16 @@ class KacRing(object):
     
     def step(self):
         # Would be great to use "map(rotate, charges)" somehow
-        for i in charges:
+        for i in self.charges:
             i.rotate()
 
     def net_charge(self):
-        return 1.0 * sum(i.spin for i in charges) / len(charges)
+        return 1.0 * sum(i.spin for i in self.charges) / len(self.charges)
 
 class Charge(object):
-    self.spin = 0 # Initialize to +1/-1 for up and down
-    self.loc = 0 # Location on the Kac Ring
-    self.kacring = None
+    spin = 0 # Initialize to +1/-1 for up and down
+    loc = 0 # Location on the Kac Ring
+    kacring = None
 
     def __init__(self, kacring, loc):
         self.spin = random.choice([-1, 1])
@@ -40,7 +38,7 @@ class Charge(object):
     def rotate(self):
         self.loc = self.loc + 1 % self.kacring.size
         if self.kacring.exists_gate_at(self.loc):
-            flip()
+            self.flip()
 
     def flip(self):
         self.spin *= -1
@@ -48,8 +46,9 @@ class Charge(object):
 def kac_graph(size, gates):
     kr = KacRing(size, gates)
     results = []
-    while (time <= 2*size):
+    while (time < 2*size):
         results.append(kr.net_charge())
+        kr.step()
         time += 1
     plt.scatter(xrange(2*size), results)
     plt.show()
